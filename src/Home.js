56eb142b1f44a6c854c15045d48fe8ca59449eec
Adrija Chatterjee <57012765+adrijachatterjee@users.noreply.github.com>
2022-09-14 23:01:38 +0530

    changes made in design

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,23 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+  const {
+    error,
+    isPending,
+    data: blogs,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
-    <div className="home">
-      { error && <div>{ error }</div> }
-      { isPending && <div>Loading...</div> }
-      { blogs && <BlogList blogs={blogs} /> }
+    <div
+      className='home'
+      style={{ display: "flex", justifyContent: "space-around" }}
+    >
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
+      <div className='designs'></div>
     </div>
   );
-}
- 
+};
+
 export default Home;
